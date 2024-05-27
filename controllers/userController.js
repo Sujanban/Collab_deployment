1964a7588ba387d6c9280e23ec1b523acc53ca63
Sujan Ban <bansujan@gmail.com>
2024-05-27 14:53:21 +0545

    feature - cleaning up user records when user deletion

--- controllers/userController.js
+++ controllers/userController.js
@@ -36,6 +36,8 @@ const deleteUser = async (req, res) => {
     await Campaign.deleteMany({ campaignOwner: userId });
     await Story.deleteMany({ userId: userId });
     await Donation.deleteMany({ userId: userId });
+    await BankAccount.deleteMany({ userId: userId });
+    await PayoutRequest.deleteMany({ userId: userId });
 
     return res.status(200).json({ message: "User deleted successfully" });
   } catch (err) {
